cd6dd845e7ab75c21fbc9b0e0bd7f07b71df6cee
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-03-23 01:21:10 +0100

    CREATE: database setup

---
+++ src/config/db/connectdb.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log('Connected to database');
+  } catch (error) {
+    console.log(`Error connecting to db, err: ${error.message}`);
+  }
+}
+
+module.exports = connectDb;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 // node modules
 const http = require('http');
 const dotenv = require('dotenv').config();
+const connectDb = require("./config/db/connectdb");
 
 // custom modules
 const app = require('./app');
@@ -10,7 +11,8 @@ const PORT = process.env.PORT || 8080;
 const server = http.createServer(app);
 
 // starting server
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+    await connectDb();
     console.log(`Server is running on port ${PORT}...`)
 })
 
