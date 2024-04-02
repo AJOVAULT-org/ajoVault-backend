601b33acc5438da1aca2c8a94794acb941572663
Daniel <oladepodaniel000@gmail.com>
2024-04-02 07:27:43 +0100

    Create: deleteUser endpoint

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/auth/register', User.Register);
 router.post('/auth/login', User.Login);
 router.patch('/auth/updateUser/:userId', User.updateUser);
+router.delete('/auth/deleteUser/:userId', User.deleteUser);
 
 module.exports = router;
