0d90396e62c7d4903b87e9fe0ccab7fc03ae716a
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-07-03 18:36:30 +0100

    removed duplicate sendmail.js file

--- src/config/email/sendMail.js
+++ src/config/email/sendMail.js
@@ -1,30 +1,20 @@
-// Importing the configured transporter object from the mailTrap.js
 const transporter = require("../mailTrap");
+require("dotenv").config();
 
 // Function to send an email using the configured transporter object
 function sendEmail(toName, toEmail, subject, text) {
-  // Creating mail options with correct recipient format, subject, and text body 
-  const mailOptions = {
-    // Sender email address
-    from: "",
-    // Recipient email address
+    const mailOptions = {
+    from: process.env.GMAIL_USER,
     to: `${toName} <${toEmail}>`,
-    // Email subject
-    subject,
-    // Email body text
-    text
+    subject: subject,
+    text: text
   };
 
-  // Return a promise for sending the email
   return new Promise((resolve, reject) => {
-    // Send the email using the transporter 
     transporter.sendMail(mailOptions, (error, info) => {
-      // Check if there's an error
       if (error) {
-        // Reject the promise with the error
         reject(new Error("Error sending email: " + error.message));
       } else {
-        // Resolve the promise with the messageID
         const successMessage = `Email sent successfully. Message ID: ${info.messageId}`;
         resolve(successMessage);
       }
