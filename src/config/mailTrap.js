4bce2ecae19845f433f028f0fcfef2bc117644b0
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-07-03 18:20:35 +0100

    Changed the SMTP server for mail delivering

--- src/config/mailTrap.js
+++ src/config/mailTrap.js
@@ -1,20 +1,22 @@
-// Importing nodemailer library for email functionality
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 require("dotenv").config();
-// Creating a transporter object for Mailtrap SMTP settings
+
 const transporter = nodemailer.createTransport({
-  // Hostname for Mailtrap SMTP server
-  host: process.env.MAIL_CLIENT_HOST,
-  // Port number for Mailtrap SMTP server
-  port: process.env.MAIL_CLIENT_PORT,
-  // Authentication credentials for Mailtrap
+  // changed SMTP to use Gmail server instead
+  service: "gmail",
   auth: {
-    // Mailtrap username
-    user: process.env.MAIL_CLIENT_USERNAME,
-    // Mailtrap password
-    pass: process.env.MAIL_CLIENT_PASSWORD
+    user: process.env.GMAIL_USER, 
+    pass: process.env.GMAIL_PASS  
   }
 });
 
-// Exporting the transporter object for use in other files
+// Verify the transporter configuration
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("Error configuring transporter:", error);
+  } else {
+    console.log("Transporter configured successfully:", success);
+  }
+});
+
 module.exports = transporter;
--- src/config/sendMail.js
+++ src/config/sendMail.js
@@ -1,30 +1,21 @@
-// Importing the configured transporter object from the mailTrap.js
 const transporter = require('./mailTrap');
+require("dotenv").config();
 
 // Function to send an email using the configured transporter object
 function sendEmail(toName, toEmail, subject, text) {
-  // Creating mail options with correct recipient format, subject, and text body 
   const mailOptions = {
-    // Sender email address
-    from: '',
     // Recipient email address
+    from: process.env.GMAIL_USER,
     to: `${toName} <${toEmail}>`,
-    // Email subject
     subject: subject,
-    // Email body text
     text: text
   };
 
-  // Return a promise for sending the email
   return new Promise((resolve, reject) => {
-    // Send the email using the transporter 
     transporter.sendMail(mailOptions, (error, info) => {
-      // Check if there's an error
       if (error) {
-        // Reject the promise with the error
         reject(new Error('Error sending email: ' + error.message));
       } else {
-        // Resolve the promise with the messageID
         const successMessage = `Email sent successfully. Message ID: ${info.messageId}`;
         resolve(successMessage);
       }
@@ -32,5 +23,4 @@ function sendEmail(toName, toEmail, subject, text) {
   });
 }
 
-// Exporting the sendEmail function for use in other files
 module.exports = sendEmail;
